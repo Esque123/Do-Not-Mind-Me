45fdc70276122b01728ff27600e20b5383a04007
Esque123 <mostert.k@gmail.com>
2024-03-06 10:20:39 +0200

    Player

---
+++ Scenes/player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED: float = 130
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	rotation = velocity.angle()
+
+
+func get_input() -> void:
+	var mv = Vector2.ZERO
+	mv.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	mv.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	velocity = mv.normalized() * SPEED
