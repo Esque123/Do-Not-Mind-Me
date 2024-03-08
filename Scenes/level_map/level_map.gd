14f3506d3393766a5b745d1866a421b6a660a021
Esque123 <mostert.k@gmail.com>
2024-03-08 11:36:07 +0200

    Detecting Exit

---
+++ Scenes/level_map/level_map.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+@onready var pickups = $Pickups
+
+
+var _pickups_count: int = 0
+var _collected: int = 0 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_pickups_count = pickups.get_children().size()
+	SignalManager.on_pickup.connect(on_pickup)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func check_show_exit() -> void:
+	if _collected == _pickups_count:
+		SignalManager.on_show_exit.emit()
+		print("on_show_exit")
+
+
+func on_pickup() -> void:
+	print("on_pickup")
+	_collected += 1
+	check_show_exit()
